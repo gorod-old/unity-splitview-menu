b94eb2cc3aef609cf3f882e2ee0f6e4be419645f
gorod-old <gorod1974@hotmail.com>
2020-01-16 15:55:51 +0300

    upload

---
+++ SplitView-menu/Assets/Scripts/SplitViewMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplitViewMenu : MonoBehaviour
+{
+
+    public Animator animator;
+    public Canvas[] content;
+    public GameObject[] indicators;
+
+    private void Start()
+    {       
+        if(!PlayerPrefs.HasKey("mode"))
+        {
+            PlayerPrefs.SetInt("mode", 0);
+        }
+        indicators[PlayerPrefs.GetInt("mode")].SetActive(true);
+    }
+
+
+    public void Click(int mode)
+    {
+        PlayerPrefs.SetInt("mode", mode);
+        for(int i = 0; i < content.Length; i++)
+        {
+            //canvas change
+            content[i].enabled = mode == i ? true : false;
+
+            //scene change
+            //SceneManager.LoadScene(mode);
+
+            indicators[i].SetActive(mode == i ? true : false);
+        }
+        animator.SetInteger("state", 2);
+    }
+
+    public void HamburgerClick()
+    {
+        animator.SetInteger("state", animator.GetInteger("state") == 1 ? 2 : 1);
+    }
+}
